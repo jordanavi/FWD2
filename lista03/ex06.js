e1865896c221f3f1a5d69762c2a01e84cf196f7a
jordanavi <jordanavilela01@gmail.com>
2019-06-04 22:06:36 -0300

    Aleteracoes nos codigos

---
+++ lista03/ex06.js
@@ -0,0 +1,40 @@
+function entrar() {
+
+    var login = document.getElementById("login").value;
+    var senha1 = document.getElementById("senha").value;
+    var confimaSenha = document.getElementById("confSenha").value;
+
+    if (login == '') { //Verifica se o Login foi digitado corretamente, ou seja, se o valor deste campo é diferente de vazio
+        alert("Campo de login vazio. Por favor, preencha os campos corretamente.");
+        return false;
+    }
+    else if (senha1 == '') { //Verifica se a senha foi digitada corretamente, ou seja, se o valor deste campo é diferente de vazio
+        alert("Campo de senha vazio. Por favor, preencha os campos corretamente.");
+        return false;
+    }
+    else
+        if (confimaSenha == '') { //Verifica se a confirmacao da senha foi digitada corretamente, ou seja, se o valor deste campo é diferente de vazio
+            alert("Campo de confirmação de senha vazio. Por favor, preencha os campos corretamente.");
+            return false;
+        }
+        else
+            if (senha1 != confimaSenha) { //verifica se as senhas são diferentes
+                alert("As senhas são diferentes. Por favor digite novamente.");
+                limpar();
+                return false;
+            }
+            else
+                if (senha1.length > 8) { //verifica o tamanho das senhas
+                    alert("A senha deve conter no máximo 8 caracteres.");
+                    limpar();
+                    return false;
+                }
+                else //sucesso
+                    alert("Login efetuado com sucesso!");
+    return true;
+}
+
+function limpar() {
+    document.getElementById("senha").value = "";
+    document.getElementById("confSenha").value = "";
+}
