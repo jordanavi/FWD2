2580fa33e2cea98d4b7876c96400e1c8fade8a82
jordanavi <jordanavilela01@gmail.com>
2019-06-17 23:14:04 -0300

    Iniciando o Js

---
+++ lista05/cronometro.js
@@ -0,0 +1,15 @@
+function playBtn(){
+    setTimeout("mudarTexto()",3000)
+}
+
+function mudarTexto(){
+    document.getElementById("paragrafo").innerHTML="" 
+}
+
+function pauseBtn(){
+    document.getElementById("paragrafo").innerHTML="Sistemas Para Internet" 
+}
+
+function stopBtn(){
+
+}
