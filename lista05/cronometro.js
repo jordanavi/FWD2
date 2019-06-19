bd32cffb634c1c693a67614d554e324307e7af77
jordanavi <jordanavilela01@gmail.com>
2019-06-19 01:06:28 -0300

    Botao play funcionando

--- lista05/cronometro.js
+++ lista05/cronometro.js
@@ -1,30 +1,33 @@
+var contaSegundo = 0;
+var contaMinuto = 0;
+var contaHora = 0;
 
-function playBtn(){
-    document.getElementById("txtHora").innerHTML="Sistemas Para Internet";
-}
-
-function resetBtn(){
-    document.getElementById("txtHora").innerHTML="00:00:00";
-}
-
-function pauseBtn(){
-    document.getElementById("txtHora").innerHTML="Sistemas Para Internet";
-}
-
-function stopBtn(){
-    document.getElementById("txtHora").innerHTML="Sistemas Para Internet";
-}
-
-function contaSegundos() {
-    setTimeout(, 1000);
+function playBtn() {
+    setInterval(
+        function(){
+            contaSegundo++;
+            if(contaSegundo<10)
+                document.getElementById("sec").innerHTML="0"+contaSegundo;
+            else
+                document.getElementById("sec").innerHTML=contaSegundo;
+            if(contaMinuto<10)
+                document.getElementById("min").innerHTML="0"+contaMinuto;
+            else
+                document.getElementById("min").innerHTML=contaMinuto;
+            if(contaHora<10)
+                document.getElementById("hor").innerHTML="0"+contaHora;
+            else
+                document.getElementById("hor").innerHTML=contaHora;
+            if(contaSegundo>=59){
+                contaSegundo=0;
+                contaMinuto++;
+            }
+            if(contaMinuto>=59){
+                contaMinuto=0;
+                contaHora++;
+            }
+        }
+    , 1000);
   }
 
 
-
-
-function mostrarHoras(){
-    var tempo = new Date();
-    document.getElementById("horas").innerHTML = tempo.getHours();
-    document.getElementById("minutos").innerHTML = tempo.getMinutes();
-    document.getElementById("segundos").innerHTML = tempo.getSeconds();
-}
