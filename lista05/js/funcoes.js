c7ea22fd313c34ea8b917d122da72f15c88996c6
jordanavi <jordanavilela01@gmail.com>
2019-07-03 21:38:40 -0300

    Inicio da lista 6

--- lista05/js/funcoes.js
+++ lista05/js/funcoes.js
@@ -1,22 +1,27 @@
+var textos = ['txt inicial','opcao 1','opcao 2','opcao 3','opcao 4','opcao 5']
+
 function trocarImg(numImg, numBolinha) {
     document.getElementById("imgMain").src = "img/img" + numImg + ".png";
     resetaBolinha();
     document.getElementById("b" + numBolinha).style.backgroundColor = "rgb(252, 252, 186)";
     document.getElementById("icon").src = "img/icon0" + numImg + ".png";
     if (numImg ==0 && numBolinha ==0){
-        document.getElementById("txt0").innerHTML = "Pet shop";
+        document.getElementById("txt0").innerHTML = textos[0];
     }
     else{
-        document.getElementById("txt0").innerHTML = document.getElementById("txt" + numImg).innerHTML;
+        document.getElementById("txt0").innerHTML = textos[numImg];
     }
     //mudarLegenda(numImg);
 }
 function resetaBolinha() {
-    document.getElementById("b1").style.backgroundColor = "rgb(108, 57, 28)";
-    document.getElementById("b2").style.backgroundColor = "rgb(108, 57, 28)";
-    document.getElementById("b3").style.backgroundColor = "rgb(108, 57, 28)";
-    document.getElementById("b4").style.backgroundColor = "rgb(108, 57, 28)";
-    document.getElementById("b5").style.backgroundColor = "rgb(108, 57, 28)";
+    for (var i=1; i<6; i++)
+        document.getElementById("b"+i).style.backgroundColor = "rgb(108, 57, 28)";
+    
+    
+    // document.getElementById("b2").style.backgroundColor = "rgb(108, 57, 28)";
+    // document.getElementById("b3").style.backgroundColor = "rgb(108, 57, 28)";
+    // document.getElementById("b4").style.backgroundColor = "rgb(108, 57, 28)";
+    // document.getElementById("b5").style.backgroundColor = "rgb(108, 57, 28)";
 }
 // function imgIni() {
 //     document.getElementById("imgMain").src = "img/img0.png";
